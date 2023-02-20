074483f324b0a2646c3388300389113b8085ac56
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-02-20 07:23:04 +0530

    35.Search Insert Position
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 35.Search Insert Position/35.Search_Insert_Position.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int first = 0;
+        int last = nums.size()-1;
+        while(first <= last)
+        {
+            int mid = (first+last)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                last = mid-1;
+            }
+            else
+            {
+                first = mid+1;
+            }
+        }
+        return last+1;
+    }
+};
