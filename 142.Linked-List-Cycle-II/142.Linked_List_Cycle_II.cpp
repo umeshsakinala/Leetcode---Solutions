97c095ba456cf783068c9620829ab0848104c0d9
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-03-09 06:39:32 +0530

    142.Linked List Cycle II
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 142.Linked-List-Cycle-II/142.Linked_List_Cycle_II.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *p = head, *q = head;
+        while (q && q->next) {
+            p = p->next;
+            q = q->next->next;
+            if (p == q) break;
+        }
+        if (!(q && q->next)) return NULL;
+        while (head != p) {
+            head = head->next;
+            p = p->next;
+        }
+        return head;
+    }
+};
