0e48b87578f8b74790e99532f04be142fab7d1a1
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-04-03 19:45:28 +0530

    881.Boats to Save People
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 881.Boats-to-Save-People/881.Boats_to_Save_People.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int count = 0, left = 0, right = people.size() - 1;
+        while(left <= right)
+        {
+            if(people[left] + people[right] <= limit)
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                right--;
+            }
+            count++;
+        }
+        return count;
+    }
+};
