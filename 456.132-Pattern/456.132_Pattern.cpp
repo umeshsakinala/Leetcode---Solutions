b46ba5f98f97fad1d0af183572b5ab66be19049e
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-09-30 18:06:17 +0530

    456.132 Pattern
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 456.132-Pattern/456.132_Pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int temp = INT_MIN;
+        stack<int> st;
+        for(int i = nums.size()-1; i >= 0; i--)
+        {
+            if(nums[i] < temp)
+            {
+                return true;
+            }
+            else while(!st.empty() && nums[i] > st.top())
+            { 
+                temp = st.top(); 
+                st.pop(); 
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
